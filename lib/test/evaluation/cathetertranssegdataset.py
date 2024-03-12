#UPDATE: fixed PCA bug, added metrics calculation

lib/test/evaluation/cathetertranssegdataset.py
@@ -30,18 +30,21 @@ class CatheterTransSegDataset(BaseDataset):
             self.simu_list = ["01", "21", "09", "29"]
             self.trim_rule = [30, 40, 30, 15]
         elif subset == 'Val':
-            self.simu_list = ["21"]
-            self.trim_rule = [30]
+            # self.simu_list = ["21"]
+            # self.trim_rule = [41]
             # self.simu_list = ["01"]
             # self.trim_rule = [50]
+            self.simu_list = ["31"]
+            self.trim_rule = [0]
             # self.simu_list = ["31"]
-            # self.trim_rule = [90]
-            # self.simu_list = ["31"]
-            # self.trim_rule = [50]
+            # self.trim_rule = [61]
 
-            # # phantom test
-            # self.simu_list = ["02"]
-            # self.trim_rule = [1]
+            # phantom test
+            phantom_simu_list = ["02", "04", "05", "06", "07", "08"]
+            self.select = 1
+            self.simu_list = [phantom_simu_list[self.select]]
+            self.trim_rule = [0]
+            self.frame_trim = [14, 285, 150, 358, 131]
 
         else:
             AttributeError("The mode is not recognized")
@@ -224,10 +227,10 @@ class CatheterTransSegDataset(BaseDataset):
         flow_w, flow_h = first_flow.shape[:2]
         init_frame_id = None
         init_bbox = None
-        frame_trim = 0          # 150 has some result
+        frame_trim = self.frame_trim[self.select] if self.select is not None else 0   #  280 # 150 has some result
         for i in range(frame_trim, len(self.bboxes_dic[class_name.split('_')[-1]][sequence_number])):
             curr_box = self.bboxes_dic[class_name.split('_')[-1]][sequence_number][i]
-            if 30*30 < curr_box[2] * curr_box[3] < flow_w * flow_h / 4:
+            if 20*20 < curr_box[2] * curr_box[3] < flow_w * flow_h / 4:
                 init_frame_id = i
                 init_bbox = self.bboxes_dic[class_name.split('_')[-1]][sequence_number][i]
                 break
@@ -237,7 +240,7 @@ class CatheterTransSegDataset(BaseDataset):
         gt_flow_path = join(seq_path.replace("filtered", "flow_cactuss_flownet2"), f"{init_frame_id:06d}.flo")
         gt_flow = torch.tensor(flow_utils.read_gen(gt_flow_path))
 
-        threshold = 0.2
+        threshold = 1   # synth: 0.2, real: 0.8
         # create a binary mask from flow
         mask_u = gt_flow.abs()[:, :, 0] > threshold
         mask_v = gt_flow.abs()[:, :, 1] > threshold

lib/test/tracker/aiareseg.py
@@ -174,7 +174,7 @@ class AIARESEG(BaseTracker):
 
     def track(self, image, info: dict = None, seq_name: str = None, segmentation: bool = None):
         H, W, _ = image[0].shape
-        self.frame_id += 1
+        self.frame_id += 5
         # debug
         print(f"Frame {self.frame_id}" + f"\tLast valid {self.last_valid_frame}")
         # Get the t-th search region
@@ -184,13 +184,12 @@ class AIARESEG(BaseTracker):
         no_mask = True
         iter = 0
         valid_mask_count_req = 4 # minimum valid masks needed to present to form a merged mask # 2
-        max_mask_search_iter = 10  # for the first hit  # 10
-        max_mask_search_iter = 4  # for the first hit  # 10
+        max_mask_search_iter = 10  # for the first hit  # 10 # 5 for real data
         addi_mask_search_overhead = 5   # chances for finding additional masks
         out_full_mask = None
         first_valid_iter = None
         enlarge_factor = 0.5
-        self.params.search_factor = 1.2     # 1.5 as start may be too big
+        self.params.search_factor = 1.5  # 1.5 as start may be too big
         while no_mask == True:
             if segmentation == True:
                 if (0 <= iter < ((addi_mask_search_overhead + first_valid_iter) if first_valid_iter is not None else max_mask_search_iter)):
@@ -198,10 +197,14 @@ class AIARESEG(BaseTracker):
                     boxes = self.x1y1x2y2_to_x1y1wh(boxes)
                     # print(f"Multishot, iter {iter}")
                     # print(boxes)
-                    if boxes[2] < 20:
-                        boxes[2] = 30
-                    if boxes[3] < 20:
-                        boxes[3] = 30
+                    if boxes[2] < 50 and torch.sum(self.state[0]) < 2000:
+                        diff = 50 - boxes[2]
+                        boxes[0] -= diff // 2
+                        boxes[2] = 50
+                    if boxes[3] < 50 and torch.sum(self.state[0]) < 2000:
+                        diff = 50 - boxes[3]
+                        boxes[1] -= diff // 2
+                        boxes[3] = 50
                     boxes = [torch.tensor(boxes)]
                     invalid_frame_tolerance = 3
                     frame_diff = 0
@@ -802,23 +805,36 @@ class AIARESEG(BaseTracker):
         """
         # Ensure mask is a binary image of type uint8
         mask = np.uint8(mask)
-        bbox = bbox.tolist()
+        # find the largest connected component on the previous mask
+        num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(prev_mask.cpu().numpy().astype(np.uint8), 4, cv2.CV_32S)
+        largest_label = np.argmax(stats[1:, -1]) + 1
+        prev_mask_largest_region = torch.tensor(labels == largest_label).float()
 
         # Find connected components
         num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
         valid_labels = [False] * num_labels
         need_pca = False
         keep_prev = False
-        if num_labels > 2:
+        keep_prev_largest = False
+        possible_labels = [i for i in range(1, num_labels)]
+        # discard regions that are too small and does not overlap with previous mask
+        for i in range(1, num_labels):
+            if torch.sum(torch.tensor(labels == i).float()) < 50 and not torch.sum(prev_mask * torch.tensor(labels == i).float()) > 0:
+                possible_labels.remove(i)
+
+        if len(possible_labels) > 1:
             # Skip the background label at index 0
             valid_labels[0] = False
             sizes = stats[1:, -1]
 
             # Step 1: max overlap with previous mask, keep all components that has overlap with previous mask
-            overlap_threshold = 0.5
-            for i in range(1, num_labels):
-                if torch.sum(prev_mask * torch.tensor(labels == i).float()) > torch.sum(torch.tensor(labels == i).float()) * overlap_threshold:
+            overlap_threshold = 0.4
+            for i in possible_labels:
+                # if torch.sum(prev_mask_largest_region * torch.tensor(labels == i).float()) > torch.sum(torch.tensor(labels == i).float()) * overlap_threshold:
+                if torch.sum(prev_mask_largest_region * torch.tensor(labels == i).float()) > torch.sum(prev_mask_largest_region) * 0.5:
                     valid_labels[i] = True
+                    # keep_prev = True
+                    # keep_prev_largest = True
 
             if not any(valid_labels):
                 need_pca = True
@@ -849,60 +865,85 @@ class AIARESEG(BaseTracker):
                 mean = pca.mean_
                 # major axis
                 major_axis = components[0]
+                minor_axis = components[1]
+                # veritcal mirror the major axis
+                major_axis[1] = -major_axis[1]
+                minor_axis[1] = -minor_axis[1]
 
                 # find centeroids of connected components
                 projected_len = []
                 centroids = np.zeros((num_labels, 2))
-                for i in range(1, num_labels):
+                for i in possible_labels:
                     if sizes[i - 1] < 50:
                         continue
                     centroids[i] = self.calculate_centroid(labels == i)
-                    dir_vec = normalize([centroids[i] - prev_center]).ravel()
-                    projected_len.append(np.abs(np.dot(dir_vec, major_axis))/np.linalg.norm(major_axis))
+                    # dir_vec = normalize([centroids[i] - prev_center]).ravel()
+                    dir_vec = centroids[i] - prev_center
+                    # project to major axis
+                    # projected_len.append(np.abs(np.dot(dir_vec, major_axis))/np.linalg.norm(major_axis))
+                    # project to minor axis
+                    projected_len.append(np.abs(np.dot(dir_vec, minor_axis)))
 
-                valid_labels[np.argmax(projected_len) + 1] = True
+                if not any(projected_len):
+                    need_pca = False
+                    keep_prev = True
+                else:
+                    valid_labels[np.argmax(projected_len) + 1] = True
+                    # valid_labels[np.argmin(projected_len) + 1] = True
+                    keep_prev_largest = True
+                    keep_prev = True
 
+            # Step 3: if there's more than 1 valid label, we remove all labels that have no overlap with previous bbox
+            if sum(valid_labels) > 1:
+                for i in possible_labels:
+                    if valid_labels[i]:
+                        if not torch.sum(torch.tensor(labels == i)[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])]) > 0:
+                            valid_labels[i] = False
 
         else:
             valid_labels[1] = True
 
-        # # Lets plot both the image and the bounding box
-        # fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-        # rgb_mask = torch.tensor(mask).unsqueeze(0).unsqueeze(0).repeat(1, 3, 1, 1)
-        # rgb_mask = rgb_mask.permute(0, 2, 3, 1).detach().cpu().numpy().astype(np.uint8) * 255
-        # # for i in range(1, num_labels):
-        # #     rgb_mask[0][labels == i] = [0, 0, 255]
-        # # plot previous mask
-        # rgb_mask[0][prev_mask == 1] = [0, 255, 0]   # green
+
+        # Lets plot both the image and the bounding box
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        rgb_mask = torch.tensor(mask).unsqueeze(0).unsqueeze(0).repeat(1, 3, 1, 1)
+        rgb_mask = rgb_mask.permute(0, 2, 3, 1).detach().cpu().numpy().astype(np.uint8) * 255
         # for i in range(1, num_labels):
-        #     if valid_labels[i]:
-        #         rgb_mask[0][labels == i] = [0, 255, 255]
-        # # rgb_mask[0][labels == largest_label] = [0, 255, 255]    # teal
-        # ax.imshow(rgb_mask[0])
-        # # rect = patches.Rectangle((int(bbox[0]), int(bbox[1])), int(bbox[2]), int(bbox[3]), linewidth=2, edgecolor='r',
-        # #                          facecolor='none')
-        # if need_pca:
-        #     # plt.scatter(centroids[1:, 1], centroids[1:, 0], c='r', s=40)
-        #     # plt.scatter(prev_center[1], prev_center[0], c='b', s=40)
-        #     plt.quiver(mean[0], mean[1], major_axis[0], major_axis[1], color='r', scale=5)  # red as major
-        #     plt.quiver(mean[0], mean[1], components[1, 0], components[1, 1], color='g', scale=5)
-        #
-        # # ax.add_patch(rect)
-        # plt.show()
+        #     rgb_mask[0][labels == i] = [0, 0, 255]
+        # plot previous mask
+        rgb_mask[0][prev_mask == 1] = [0, 255, 0]   # green
+        for i in possible_labels:
+            if valid_labels[i]:
+                rgb_mask[0][labels == i] = [0, 255, 255]
+        # rgb_mask[0][labels == largest_label] = [0, 255, 255]    # teal
+        ax.imshow(rgb_mask[0])
+        rect = patches.Rectangle((int(bbox[0]), int(bbox[1])), int(bbox[2]), int(bbox[3]), linewidth=2, edgecolor='r',
+                                 facecolor='none')
+        if need_pca:
+            plt.scatter(centroids[1:, 1], centroids[1:, 0], c='r', s=40)
+            plt.scatter(prev_center[1], prev_center[0], c='b', s=40)
+            plt.quiver(mean[0], mean[1], major_axis[0], major_axis[1], color='r', scale=5)  # red as major
+            plt.quiver(mean[0], mean[1], minor_axis[0], minor_axis[1], color='g', scale=5)
+
+        ax.add_patch(rect)
+        plt.show()
 
         # Create a new mask for the largest connected component
         largest_component = np.zeros_like(mask)
-        for i in range(1, num_labels):
+        for i in possible_labels:
             if valid_labels[i]:
                 largest_component[labels == i] = 1.0
         # largest_component[labels == largest_label] = 1.0
         largest_component = torch.tensor(largest_component).float()
 
         # If the largest component is too small, keep the previous mask
-        if torch.sum(largest_component) < 100:
+        if torch.sum(largest_component) < 200:
             keep_prev = True
         if keep_prev:
-            largest_component = torch.max(largest_component, prev_mask)
+            if keep_prev_largest:
+                largest_component = torch.max(largest_component, prev_mask_largest_region)
+            else:
+                largest_component = torch.max(largest_component, prev_mask)
         return largest_component
 def get_tracker_class():
     return AIARESEG

lib/train/base_functions.py
@@ -71,7 +71,7 @@ def build_dataloaders(cfg, settings):
                                     tfm.RandomHorizontalFlip_Norm(probability=0.5),
                                     tfm.RandomRotation(probability=0.3),
                                     #tfm.RandomCropping(probability=0.1),
-                                    tfm.Gaussian_Blur(probability=0.3),
+                                    # tfm.Gaussian_Blur(probability=0.3),
                                     tfm.Salt_and_pepper(probability=0.5),
                                     tfm.Normalize(mean=cfg.DATA.MEAN, std=cfg.DATA.STD)
                                     )
@@ -156,9 +156,10 @@ def get_optimizer_scheduler(net, cfg):
     optimizer = torch.optim.AdamW(param_dicts, lr=cfg.TRAIN.LR, weight_decay=cfg.TRAIN.WEIGHT_DECAY)
 
     if cfg.TRAIN.SCHEDULER.TYPE == 'cosine':
-        iter_per_epoch = int(np.floor(cfg.DATA.TRAIN.SAMPLE_PER_EPOCH / cfg.TRAIN.BATCH_SIZE))
+
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=cfg.TRAIN.SCHEDULER.T0_EPOCH,
                                                                             T_mult=cfg.TRAIN.SCHEDULER.T_MULT, eta_min=cfg.TRAIN.SCHEDULER.MIN_LR)
+        # iter_per_epoch = int(np.floor(cfg.DATA.TRAIN.SAMPLE_PER_EPOCH / cfg.TRAIN.BATCH_SIZE))
         # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg.TRAIN.EPOCH * iter_per_epoch,
         #                                                           eta_min=cfg.TRAIN.SCHEDULER.MIN_LR)
     elif cfg.TRAIN.SCHEDULER.TYPE == 'step':

lib/train/data/generate_bboxes.py
@@ -79,8 +79,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    root_dir = os.path.join(args.dataset)
-    # root_dir = os.path.join('/media/liming/Data/IDP/dataset/us_phantom')
+    # root_dir = os.path.join(args.dataset)
+    root_dir = os.path.join('/media/liming/Data/IDP/dataset/us_phantom_eval')
     # out_file = os.path.join(root_dir, "bboxes")
     flow_folder_name = "flow_cactuss_flownet2"
     store = False
@@ -105,6 +105,8 @@ if __name__ == '__main__':
         for seq_dir_name in tqdm(os.listdir(cath_dir)):
             seq_dir = os.path.join(cath_dir, seq_dir_name)
             seq_num = int(seq_dir_name.split('_')[-1])
+            # sequence_len = len(os.listdir(seq_dir))
+            # bboxes = torch.zeros((100, sequence_len, 4), device=device)
             for file in os.listdir(seq_dir):
                 if file.endswith('.flo'):
                     file_num = int(file.split('_')[-1].split('.')[0])

lib/train/trainers/base_trainer.py
@@ -37,6 +37,7 @@ class BaseTrainer:
 
         self.epoch = 0
         self.stats = {}
+        self.init_epoch = 0
 
         self.device = getattr(settings, 'device', None)
         if self.device is None:
@@ -86,8 +87,10 @@ class BaseTrainer:
             try:
                 if load_latest:
                     # self.load_checkpoint(checkpoint=self.settings.env.pretrained_networks)
-                    max_epochs = max_epochs + 500
                     self.load_checkpoint()
+                    self.init_epoch = self.epoch
+                    max_epochs = max_epochs + self.epoch
+
 
                     # Here, you may decide to remove some of the weights in the final layer in order to get better training performance via transfer learning
 
@@ -274,6 +277,20 @@ class BaseTrainer:
                      'lr': self.cfg.TRAIN.LR * self.cfg.TRAIN.BACKBONE_MULTIPLIER}
                 ]
                 self.optimizer = torch.optim.AdamW(param_dicts, lr=self.cfg.TRAIN.LR, weight_decay=self.cfg.TRAIN.WEIGHT_DECAY)
+
+                if self.cfg.TRAIN.SCHEDULER.TYPE == 'cosine':
+
+                    self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(self.optimizer,
+                                                                                        T_0=self.cfg.TRAIN.SCHEDULER.T0_EPOCH,
+                                                                                        T_mult=self.cfg.TRAIN.SCHEDULER.T_MULT,
+                                                                                        eta_min=self.cfg.TRAIN.SCHEDULER.MIN_LR)
+                    # iter_per_epoch = int(np.floor(self.cfg.DATA.TRAIN.SAMPLE_PER_EPOCH / self.cfg.TRAIN.BATCH_SIZE))
+                    # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.cfg.TRAIN.EPOCH * iter_per_epoch,
+                    #                                                           eta_min=self.cfg.TRAIN.SCHEDULER.MIN_LR)
+                elif self.cfg.TRAIN.SCHEDULER.TYPE == 'step':
+                    self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, self.cfg.TRAIN.LR_DROP_EPOCH)
+                else:
+                    raise ValueError('Unknown scheduler type: %s' % self.cfg.TRAIN.SCHEDULER.TYPE)
             elif key == 'stats':
                 continue
             else:

visualisation/inferencing_sequence_segmentation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 from PIL import Image
 import monai
+import cv2 as cv
 
 # In this script we will visualise the results after we pass the sequences through the AiA track framework
 # The inferencing from the model will not be done in real time, we will implement that in another script instead
@@ -56,7 +57,7 @@ def open_mask(mask_path, seq_no='51'):
     Outputs a list full of the ground truth bounding boxes for each of the images of the sequence
     """
 
-    mask_folder_path = os.path.join(mask_path,f'Catheter-{seq_no}')
+    mask_folder_path = os.path.join(mask_path,f"label0031_%02d"%(int(seq_no)))
 
     names, files = get_names(mask_folder_path)
     mask_list = []
@@ -74,7 +75,7 @@ def open_mask(mask_path, seq_no='51'):
     return torch.stack([mask for mask in mask_list], dim=0)
 
 def open_model_mask(mask_path, seq_no='51'):
-    mask_folder_path = os.path.join(mask_path,f'Catheter-{seq_no}')
+    mask_folder_path = os.path.join(mask_path,f"label0031_%02d"%(int(seq_no)))
 
     names, files = get_names(mask_folder_path)
     mask_list = []
@@ -93,7 +94,7 @@ def open_model_mask(mask_path, seq_no='51'):
 def open_images(image_path, seq_no='51'):
 
     #image_folder_path = os.path.join(image_path, 'Catheter', f'Catheter-{seq_no}', 'img')
-    image_folder_path = os.path.join(image_path, 'Catheter', f'Catheter-{seq_no}')
+    image_folder_path = os.path.join(image_path, f"label0031_%02d"%(int(seq_no)))
     names, files = get_names(image_folder_path)
     image_list = []
     files = sorted(files)
@@ -137,42 +138,82 @@ def loop_image(image_tensor, gt_mask_tensor, model_mask_tensor, id):
 
         model_mask = model_mask.permute(1, 2, 0).repeat(1, 1, 3).numpy()
         model_mask = np.where(model_mask == 1, [255, 0, 0], [0, 0, 0]).astype(float) / 255.0
+        gt_mask = gt_mask.permute(1, 2, 0).repeat(1, 1, 3).numpy()
+        gt_mask = np.where(gt_mask == 1, [0, 255, 0], [0, 0, 0]).astype(float) / 255.0
         output2 = image
         output2 = cv2.addWeighted(output2, 1, model_mask, 0.5, 0)
+        output2 = cv2.addWeighted(output2, 1, gt_mask, 0.5, 0)
         plt.figure(figsize=(15,15))
         ax = plt.gca()
         plt.imshow(output2)
         ax.set_axis_off()
-        #plt.show()
+        plt.show()
 
 
-        save_dir = f'/home/atr17/Desktop/AiA results/AiAReSeg_catheter/{id}'
-        #
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-        plt.savefig(os.path.join(save_dir,f"{i}.png"))
+        # save_dir = f'/media/liming/Data/IDP/dataset/results/aligned_jit_step_560/{id}'
+        # #
+        # if not os.path.exists(save_dir):
+        #     os.mkdir(save_dir)
+        # plt.savefig(os.path.join(save_dir,f'{i:06d}.png'))
         plt.close()
 
 
 if __name__ == '__main__':
 
+    gt_mask_path = '/media/liming/Data/IDP/dataset/us_simulation3_cactuss/rotations_transverse_31/filtered_masks'
+    image_path = '/media/liming/Data/IDP/dataset/us_simulation3_cactuss/rotations_transverse_31/filtered'
 
-    # First lets load the masks
-    gt_mask_path = '/media/atr17/HDD Storage/US_axial_data_shallower_V2/Masks/Val'
-    model_mask_path = '/home/atr17/PhD/Research/Phase_5_Al_model_building/DETR_net/AiASeg_w_temp/AiATrack/tracking/PATH/AiATrack/test/tracking_results/aiatrack/AiASeg_s+p'
-    image_path = "/media/atr17/HDD Storage/US_axial_data_shallower_V2/Images/Val"
+    # model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/rotations_transverse_31/aligned_step_584'
+    # model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/rotations_transverse_31/misaligned_0585'
+    # model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/rotations_transverse_31/misaligned_0454'
+    model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/rotations_transverse_31/aligned_jit_step_560'
+    # model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/rotations_transverse_31/filtered'
+
+    # phantom_list = ["02", "04", "05", "06", "07", "08"]
+    # phantom_frame_trim = [0, 0, 0, 0, 0, 0]
+    # chosen = 0
+    # model_pred_folder_name = 'filtered'
+    #
+    # gt_mask_path = '/media/liming/Data/IDP/dataset/us_phantom/phantom_transverse_' + phantom_list[chosen] + '/filtered_masks'
+    # image_path = '/media/liming/Data/IDP/dataset/us_phantom/phantom_transverse_' + phantom_list[chosen] + '/filtered'
+    # model_mask_path = '/media/liming/Data/IDP/AiAProj/AiAReSeg/test/tracking_results/aiareseg/AiASeg_unsupervised/phantom_transverse_' + phantom_list[chosen] + '/' + model_pred_folder_name
+
 
     dice = monai.losses.DiceLoss(jaccard=False)
     mae = monai.metrics.MAEMetric()
 
     dice_list = []
     mae_list = []
-    for i in range(1, 6):
+    # count subfolders in model_mask_path
+    folder_count = sum(os.path.isdir(os.path.join(model_mask_path, entry)) for entry in os.listdir(model_mask_path))
+    for i in range(0, folder_count):
         seq_no = str(i)
         gt_mask_tensor = open_mask(gt_mask_path, seq_no=seq_no).unsqueeze(1)
         model_mask_tensor = open_model_mask(model_mask_path, seq_no=seq_no).unsqueeze(1)
-        gt_mask_tensor = torch.where(gt_mask_tensor==2,1,0)
+        # model_mask_tensor = torch.roll(model_mask_tensor, shifts=10, dims=2)
+        # model_mask_tensor = torch.roll(model_mask_tensor, shifts=10, dims=3)
+        if gt_mask_tensor.max() == 2:
+            gt_mask_np = gt_mask_tensor.numpy()
+            # gt_mask_tensor of shape (B, C, H, W)
+            # do some dialation on the catheter
+            kernel = 2 * np.ones((10, 10), np.uint8)
+            gt_mask_tensor = (gt_mask_np == 2).astype(np.uint8)  # Isolate the catheter
+            # dilate the gt_mask_tensor using the kernel
+            for j in range(gt_mask_tensor.shape[0]):
+                gt_mask_tensor[j, 0, :, :] = cv2.dilate(gt_mask_tensor[j, 0, :, :], kernel, iterations=2)
+            gt_mask_tensor = torch.tensor(gt_mask_tensor)
+            # vertical flip gt_mask_tensor of shape (B, C, H, W)
+            gt_mask_tensor = torch.flip(gt_mask_tensor, [2])
+            # move the catheter up by 5 pixels
+            gt_mask_tensor = torch.roll(gt_mask_tensor, shifts=-10, dims=2)
+
+        # gt_mask_tensor = torch.where(gt_mask_tensor==2,1,0)
         image_tensor = open_images(image_path, seq_no=seq_no)
+        if gt_mask_tensor.shape[0] > model_mask_tensor.shape[0]:
+            frame_diff = gt_mask_tensor.shape[0] - model_mask_tensor.shape[0]
+            # Pad the model mask tensor at the start
+            model_mask_tensor = torch.cat([torch.zeros(frame_diff, *model_mask_tensor.shape[1:]), model_mask_tensor], dim=0)
+
 
         dice_list.append(dice(gt_mask_tensor, model_mask_tensor).item())
         mae_list.append(torch.mean(mae(gt_mask_tensor, model_mask_tensor)).item())
@@ -180,13 +221,47 @@ if __name__ == '__main__':
         loop_image(image_tensor, gt_mask_tensor, model_mask_tensor, id=i)
     dice_list = np.array(dice_list)
     average_dice = np.mean(dice_list)
+    sd_dice = np.std(dice_list)
+
     mae_list = np.array(mae_list)
     average_mae = np.mean(mae_list)
+    sd_mae = np.std(mae_list)
+
     print("Done")
     print(f"Dice_loss:{average_dice}")
+    print(f"SD Dice_loss:{sd_dice}")
     print(f"MAE: {average_mae}")
+    print(f"SD MAE: {sd_mae}")
     # Performance 0.0808, dice:0.9192 , average mae:0.002134
     # Catheter performance, dice: 0.831, average mae: 0.000138
 
     # TODO: Loop through each image, and then overlay the gt and the model outputs on the same image
+    # misaligned 0585 -- 1 iter -- after shifting:
+    # Dice_loss:0.44543371440433877
+    # MAE: 0.003878319211807826
+    # misaligned 0585 -- 2 iter -- after shifting:
+    # Dice_loss: 0.37824380216075154
+    # MAE: 0.00394323936269779
 
+    # misaligned 0454 -- 1 iter -- after shifting:
+    # Dice_loss: 0.4512748546534922
+    # MAE: 0.0041396369657687926
+    # misaligned 0454 -- 2 iter -- after shifting:
+    # Dice_loss: 0.3769470280626925
+    # MAE: 0.004078182061431751
+
+    # aligned_step_0584 -- 1 iter:
+    # Dice_loss: 0.6590016681428362
+    # MAE: 0.003393401719998709
+
+    # aligned_cosine_630 -- 1 iter:
+    # Dice_loss: 0.7015915868076973
+    # MAE: 0.0034247522353991105
+
+    # aligned_jit_step_560 -- 1 iter:
+    # Dice_loss:0.45517472800139974
+    # MAE: 0.0035046001304158934
+
+    # aligned_jit_step_560 -- 2 iter:
+    # Dice_loss: 0.4211802001951671
+    # MAE: 0.003954319072673991